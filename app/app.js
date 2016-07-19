5e607c72920fae058be04f5a8de9a753d8331c2d
Mathieu Débit <mathieu.debit@gmail.com>
2016-07-19 10:00:01 +0200

    fix(app): migrate to LocaleStorage
    - compatible with Firefox

--- app/app.js
+++ app/app.js
@@ -71,7 +71,7 @@ install();
 import localforage from 'localforage';
 
 localforage.config({
-  driver: localforage.WEBSQL, // Force WebSQL; same as using setDriver()
+  driver: localforage.LOCALSTORAGE, // Force WebSQL; same as using setDriver()
   name: 'rise2016-app',
   version: 1.0,
   size: 4980736, // Size of database, in bytes. WebSQL-only for now.
