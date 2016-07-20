06aa5ce23ba4484491f08aa3d5daa652144a6d40
Mathieu Débit <mathieu.debit@gmail.com>
2016-07-20 19:25:26 +0200

    feat(app): add Live page content

--- app/containers/Live/index.js
+++ app/containers/Live/index.js
@@ -5,23 +5,82 @@
 */
 
 import React from 'react';
+import localforage from 'localforage';
+import BaseAPI from 'api/base.js';
 import Header from '../Header';
 
 import styles from './styles.css';
+import AppStyles from 'containers/App/styles.css';
+import loader from '../App/img/loader.png';
 
-class Live extends React.Component { // eslint-disable-line react/prefer-stateless-function
+class Live extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      data: null,
+      isLoading: false,
+      hasError: false,
+    };
+  }
+
+  componentDidMount() {
+    this.init();
+  }
+
+  init() {
+    this.setState({ isLoading: true });
+
+    localforage.getItem('userLocale').then((locale) => {
+      BaseAPI.getPosts(locale, 'live')
+        .then((response) => {
+          const posts = response.data.posts;
+
+          if (posts[0]) {
+            return this.setState({ isLoading: false, data: posts[0], hasError: false });
+          }
+
+          return this.setState({ isLoading: false, hasError: true });
+        })
+        .catch(() => {
+          this.setState({ isLoading: false, hasError: true });
+        });
+    });
+  }
+
   render() {
     return (
       <div className={styles.live}>
         <Header backButton />
 
         <div className={styles.container}>
+          {
+            this.state.isLoading &&
+              <div className={AppStyles.loading}>
+                <img src={loader} alt="" />
+              </div>
+          }
+
           <h2 className={styles.headerTitle}>
-            Live
+          Live
           </h2>
-          <p>
-            Be ready! Stay tuned to see live streamings of the session!
-          </p>
+
+          {
+            this.state.hasError &&
+              <p>Oops, something went wrong...</p>
+          }
+
+          {
+            this.state.data &&
+              <div>
+                <h2 className={styles.headerTitle}>
+                  {this.state.data.title}
+                </h2>
+
+                <div dangerouslySetInnerHTML={{ __html: this.state.data.content }} />
+              </div>
+          }
+
         </div>
       </div>
     );
